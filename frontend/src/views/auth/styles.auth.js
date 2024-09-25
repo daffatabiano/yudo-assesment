65d26f59c2ed8624ff2311de8833afdc805a5123
unknown <daffatabianoo@gmail.com>
2024-09-25 14:19:20 +0700

    (add(auth): styles refactoring

---
+++ frontend/src/views/auth/styles.auth.js
@@ -0,0 +1,34 @@
+export const styles = {
+  input: {
+    width: '100%',
+    padding: '10px',
+    borderRadius: '5px',
+    border: '1px solid #ccc',
+  },
+  button: {
+    width: '100%',
+    padding: '10px',
+    borderRadius: '5px',
+    backgroundColor: '#007bff',
+    color: '#fff',
+    border: 'none',
+    cursor: 'pointer',
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '10px',
+    minHeight: '100vh',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    padding: '0 10px',
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '15px',
+    width: '100%',
+    maxWidth: '300px',
+  },
+};
