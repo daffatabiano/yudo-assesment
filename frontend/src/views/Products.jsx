8fd337800e32e50fa318f4fc4bc2b01778b3d93d
unknown <daffatabianoo@gmail.com>
2024-09-25 21:31:59 +0700

    add(frontend): products view dashboard

---
+++ frontend/src/views/Products.jsx
@@ -0,0 +1,61 @@
+import LayoutDashbaord from '../layout/LayoutDashboard';
+import { styles } from './views.style';
+
+export default function Products() {
+  return (
+    <LayoutDashbaord>
+      <div
+        style={{
+          ...styles.container,
+          alignItems: 'flex-start',
+          justifyContent: 'flex-start',
+          paddingLeft: '250px',
+          gap: '20px',
+          display: 'flex',
+        }}>
+        <div
+          style={{
+            width: '100%',
+            display: 'flex',
+            justifyItems: 'space-between',
+            alignItems: 'center',
+            padding: '20px',
+            gap: '100px',
+          }}>
+          <h1>Products Control</h1>
+          <button style={{ ...styles.button, width: '200px' }}>
+            Add Product
+          </button>
+        </div>
+        <div style={{ width: '100%', backgroundColor: 'red' }}>
+          <div style={{ ...styles.card, minHeight: '40vh' }}>
+            <p style={{ margin: '0', fontWeight: 'bold', textAlign: 'center' }}>
+              Total : 0
+            </p>
+            <div
+              style={{
+                position: 'relative',
+                width: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '5px',
+                padding: '10px',
+              }}>
+              <div
+                style={{
+                  position: 'absolute',
+                  top: '5px',
+                  right: '5px',
+                  width: '20px',
+                  height: '20px',
+                  backgroundColor: 'red',
+                  borderRadius: '50%',
+                  display: 'flex',
+                }}></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </LayoutDashbaord>
+  );
+}
